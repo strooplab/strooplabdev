074db143ff3cd7fd4d72e3f7a084e510225a8cf8
strooplab <jdgarciao1@miusbctg.edu.co>
2024-07-13 19:25:03 -0500

    added Django Portfolio

---
+++ main/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import ContactProfile
+
+class ContactForm(forms.ModelForm):
+
+        name = forms.CharField(max_length=100, required=True,
+                widget = forms.TextInput(attrs = {
+                        'placeholder': '*Full Name: ',
+                        'class': 'form-control'
+                        }))
+        email = forms.EmailField(max_length=254, required=True,
+                widget = forms.EmailInput(attrs = {
+                        'placeholder': '*Email: ',
+                        'class': 'form-control'
+                        }))
+        message = forms.CharField(max_length=1000, required=True,
+                widget=forms.Textarea(attrs = {
+                        'placeholder': '*Message: ',
+                        'class': 'form-control'
+                        }))
+        
+        class Meta:
+            model = ContactProfile
+            fields = {'name', 'email', 'message',}
